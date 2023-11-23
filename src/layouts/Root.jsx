ce373380a36a7412bd678c54f03ddb354cc4dff0
ahosanhabib2121 <ahosanhabib21021@gmail.com>
2023-11-24 01:15:27 +0600

    project install and file setup

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+    return (
+        <div>
+            
+            {/* outlet here */}
+            <Outlet/>
+        </div>
+    );
+};
+
+export default Root;
