ed5587bc4a019f5e963e3d66538fcd84cd2f2689
ahosanhabib2121 <ahosanhabib21021@gmail.com>
2023-11-27 20:34:56 +0600

    all classes page created and data shwon in to the database

---
+++ src/components/share/BannerTitle.jsx
@@ -0,0 +1,21 @@
+
+const BannerTitle = ({title, subTitle}) => {
+    return (
+        <div>
+            <div className='relative '>
+                <div className="hero h-[350px] overflow-hidden">
+                    <div className="hero-content">
+                        <div className='-mt-10'>
+                            <h1 className="text-5xl md:text-6xl text-center font-bold mb-4">{title} </h1>
+                            <p className="text-center text-lg max-w-lg">{subTitle}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className=" absolute bottom-0 right-0 w-4/6 min-h-screen bg-gradient-to-tl from-[#9050cce3] to-transparent via-transparent opacity-60 rounded-br-[40px] "></div>
+            </div>
+        </div>
+    );
+};
+
+export default BannerTitle;
--- src/pages/allClasses/AllClasses.jsx
+++ src/pages/allClasses/AllClasses.jsx
@@ -1,10 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
+import Button from "../../components/button/Button";
+import BannerTitle from "../../components/share/BannerTitle";
 import Container from "../../components/share/Container";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import loader from '../../assets/loader.gif';
 
 const AllClasses = () => {
+    const axiosPublic = useAxiosPublic();
+    const { data: allCalss, isLoading } = useQuery({
+        queryKey: ['allCalss'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/addClass');
+            return res.data;
+        }
+
+    });
+
+    if (isLoading) {
+        return <div className=" grid justify-center items-center h-screen">
+            <img src={loader} alt="not found" />
+        </div>
+    }
+
     return (
         <div className=" pt-28">
             <Container>
-                <h2>AllClasses page here</h2>
+                {/* BannerTitle section here */}
+                <BannerTitle
+                    title={'See all class'}
+                    subTitle={'Enroll your IT potential with ByteSkill today and embark on a journey of learning and growth!'}
+                />
+                <div className=" mt-8">
+                    <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                        {
+                            allCalss.map(data => <div key={data?._id} className="card card-compact bg-slate-700 shadow-xl">
+                                <figure><img src={data?.image} className=" w-full h-60 object-cover" alt="not found" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{data?.title}</h2>
+                                    <p>Name: {data?.name}</p>
+                                    <p>Price: ${data?.price}</p>
+                                    <div className="card-actions justify-end mt-3">
+                                        <Button
+                                            label={'Enroll'}
+                                        />
+                                    </div>
+                                </div>
+                            </div>)
+                        }
+                    </div>
+                </div>
             </Container>
         </div>
     );
